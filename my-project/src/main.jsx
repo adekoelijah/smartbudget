60af125242536d0c92c0cd5d28ad45ca2acafd9d
adeko ayomide <adekoelijah@gmail.com>
2026-05-16 19:02:21 +0100

    Fix frontend build issue

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
+import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthProvider";
 
