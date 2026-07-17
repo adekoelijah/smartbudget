5082355e994be3bee8041244211171319358b4b7
adeko ayomide <adekoelijah@gmail.com>
2026-07-17 12:36:58 +0100

    update url

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "";
+  "https://smartbudgets.vercel.app/";
 // const API_BASE_URL =
 //   import.meta.env.VITE_API_URL ||
 //   "http://localhost:5000/api";
