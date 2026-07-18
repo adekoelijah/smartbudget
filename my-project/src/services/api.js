eadbbbf6112fb11238080f618b37c9bb208d9821
adeko ayomide <adekoelijah@gmail.com>
2026-07-18 14:08:21 +0100

    add api to the url of the backend in the service api

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "https://smartbudgets.vercel.app/";
+  "https://smartbudgets.vercel.app/api";
   
 // const API_BASE_URL =
 //   import.meta.env.VITE_API_URL ||
