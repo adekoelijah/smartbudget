b6c0006442d6f82fd36ae2f30c7ecd0dc444eb14
adeko ayomide <adekoelijah@gmail.com>
2026-07-19 23:01:07 +0100

    update the server for environment variable

--- smartbudget-backend/server.js
+++ smartbudget-backend/server.js
@@ -4,8 +4,13 @@ import http from "http";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 import { initSocket } from "./src/socket/socket.js";
-
+ 
 dotenv.config();
+// new lines of code
+console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
+console.log("JWT_SECRET value:", process.env.JWT_SECRET ? "Loaded" : "Undefined");
+
+// new lines of code
 
 const PORT = process.env.PORT || 5000;
 
